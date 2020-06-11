d8ab52c3f33f936eb476b88f4ea24146b5171e6a
Shiheng Dong <dongshiheng9@gmail.com>
2020-06-10 23:17:15 -0400

    update

---
+++ todo-app/functions.js
@@ -0,0 +1,56 @@
+//Fetch existing todos from localStorage
+const getSavedTodos=function() {
+    const todosJSON = localStorage.getItem('todos')
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON)
+    }
+    else{
+        return []
+    }
+}
+
+//save todos to localStorage
+const saveTodos=function (todos) {
+    localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+//Render application todos based on filters
+const renderTodos=function(todos,filerText){
+    let filteredTodes=todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filterText.searchText.toLowerCase())
+    })
+
+    filteredTodes=filteredTodes.filter(function(todo){
+        if(filterText.hideCompleted===true){
+            return todo.completed===false
+        }
+        else{
+            return todo
+        }
+    })
+
+    const incompleteTodos=todos.filter(function(todo){
+        return !todo.completed
+    })
+
+    document.querySelector('#div-filter').innerHTML=''
+    document.querySelector('#div-filter').appendChild(generateSummaryDOM(incompleteTodos))
+
+    filteredTodes.forEach(function(element){
+        document.querySelector('#div-filter').appendChild(generateTodoDOM(element))
+    })
+}
+
+//Get the DOM elements for an individual note
+const generateTodoDOM=function (element) {
+    const p=document.createElement('p')
+    p.textContent=element.text
+    return p
+}
+
+//Get the DOM elements for list summary
+const generateSummaryDOM=function(incompleteTodos){
+    const summary=document.createElement('h2')
+    summary.textContent=`You have ${incompleteTodos.length} todos left.`
+    return summary
+}
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,58 +1,11 @@
-const  todos=[]
-todos[0]={text:'walk the dog',completed:true}
-todos[1]={text:'swimming',completed:false}
-todos[2]={text:'shopping',completed:true}
-todos[3]={text:'cooking',completed:false}
-todos[4]={text:'turn off the light',completed:true}
-
-/*const ps=document.querySelectorAll('p')
-ps.forEach(function (p) {
-    if(p.textContent.includes('the')){
-        p.remove()
-    }
-})*/
+let  todos=getSavedTodos()
 
 const filterText={
     searchText:'',
     hideCompleted:false
 }
 
-const renderTodos=function(todos,filerText){
-    let filteredTodes=todos.filter(function (todo) {
-        return todo.text.toLowerCase().includes(filterText.searchText.toLowerCase())
-    })
-
-    filteredTodes=filteredTodes.filter(function(todo){
-        if(filterText.hideCompleted===true){
-            return todo.completed===false
-        }
-        else{
-            return todo
-        }
-    })
-
-    const incompleteTodos=todos.filter(function(todo){
-        return !todo.completed
-    })
-
-
-    document.querySelector('#div-filter').innerHTML=''
-
-    const h2=document.createElement('h2')
-    h2.textContent=`You have ${incompleteTodos.length} todos left.`
-    document.querySelector('#div-filter').appendChild(h2)
-
-
-    filteredTodes.forEach(function(element){
-        const p=document.createElement('p')
-        p.textContent=element.text
-        document.querySelector('#div-filter').appendChild(p)
-    })
-}
-
 renderTodos(todos,filterText)
-
-
 
 document.querySelector('#filter-todo').addEventListener('input',function (e) {
     filterText.searchText=e.target.value
@@ -66,16 +19,13 @@ document.querySelector('#add-things-todo').addEventListener('submit',function(e)
         text:e.target.elements.text.value,
         completed:false
     })
-
+    saveTodos(todos)
     renderTodos(todos,filterText)
     e.target.elements.text.value=''
 
-    localStorage.setItem('todos',JSON.stringify(todos))
 })
 
 document.querySelector('#check-uncompleted').addEventListener('click',function(e){
     filterText.hideCompleted=e.target.checked
     renderTodos(todos,filterText)
 })
-
-
