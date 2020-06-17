1f4602e84b38da450d0f3b4372c964fd68c57a16
Shiheng Dong <dongshiheng9@gmail.com>
2020-06-16 22:38:57 -0400

    update

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -2,8 +2,18 @@ const titleElement=document.querySelector('#note-title')
 const bodyElement=document.querySelector('#note-body')
 const removeElement=document.querySelector('#remove-note')
 
+const getSavednotes=function() {
+    const notesJSON = localStorage.getItem('todos')
+    if (notesJSON !== null) {
+        return JSON.parse(notesJSON)
+    }
+    else{
+        return []
+    }
+}
+
 const noteId=location.hash.substring(1)
-let notes=getSavedTodos()
+let notes=getSavednotes()
 // console.log(notes)
 let note=notes.find(function(note){
     return note.id===noteId
@@ -12,25 +22,38 @@ let note=notes.find(function(note){
 if (note===undefined){
     location.assign('index.html')
 }
+//
+const removeNote=function (id) {
+    const noteIndex=notes.findIndex(function(note){
+        return note.id===id
+    })
+    if(noteIndex>-1){
+        notes.splice(noteIndex,1)
+    }
+}
+//
+const saveNotes=function (notes) {
+    localStorage.setItem('todos',JSON.stringify(notes))
+}
 
 titleElement.value= note.text
 bodyElement.value=note.body
 
 titleElement.addEventListener('input',function(e){
     note.text=e.target.value
-    saveTodos(notes)
+    saveNotes(notes)
 })
 
 bodyElement.addEventListener('input',function(e){
     note.body=e.target.value
-    saveTodos(notes)
+    saveNotes(notes)
 })
 
-// removeElement.addEventListener('click',function(e){
-//     removeTodo(noteId)
-//     saveTodos(notes)
-//     location.assign('index.html')
-// })
+removeElement.addEventListener('click',function(e){
+    removeNote(noteId)
+    saveNotes(notes)
+    location.assign('index.html')
+})
 
 window.addEventListener('storage',function(e){
     if (e.key==='todos'){
